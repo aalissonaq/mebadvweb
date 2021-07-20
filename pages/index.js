54ec7cc9ddbea60d97511f24c01499da03ca531c
aalissonaq <aalissonalmeidaq@gmail.com>
2021-07-20 00:45:46 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 // ;
 import { Box } from '@chakra-ui/react';
-import useAuth from '../hooks/useAuth';
+// import useAuth from '../hooks/useAuth';
 /* import styles from '../styles/Home.module.css' */
 
 export default function Home() {
-  const { signin } = useAuth();
+  // const { signin } = useAuth();
   return (
     <Box bg="gray.100" w="100%" p={4}>
       This is the Box
